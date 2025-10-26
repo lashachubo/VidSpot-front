39a9a7e4bbd6250174b1edd31ddca568730aac8e
lashachubo <lashachubinidz3@gmail.com>
2025-10-26 23:57:13 +0400

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <footer className="bg-white shadow-inner py-3 text-center text-gray-500 text-sm">
-      © {new Date().getFullYear()} Footer placeholder
+      © {new Date().getFullYear()} Footer
     </footer>
   );
 };
